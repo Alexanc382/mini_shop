c6dc94592a04f94fd9693744a1703dbad3a0d9d4
Alexanc382 <alexanc382@gmail.com>
2025-08-19 08:02:09 +0300

    1. запущен код init_db.py

--- database/init_db.py
+++ database/init_db.py
@@ -1,11 +1,11 @@
 import sqlite3
 
-conn = sqlite3.connect('shop.db')
+conn = sqlite3.connect('new_shop.db')
 cur = conn.cursor()
 
 # таблица товаров
 cur.execute('''
-CREATE TABLE IN NOT EXISTS products (
+CREATE TABLE IF NOT EXISTS products (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     description TEXT,
@@ -14,12 +14,17 @@ CREATE TABLE IN NOT EXISTS products (
     )
 ''')
 
-# добавляем товары
+# добавлены товары
 cur.executemany('''
 INSERT INTO products (name, description, price, image)
 VALUES (?, ?, ?, ?) 
  ''', [
-    ('Красные розы','небольшой букет вечной классики', 7500,'images\\red_roses.jpg')
-    ('Желтые тюльпаны','свежие, яркие тюльпына', 6500,'images\\yellow_tulips.jpeg')
-    ('Синие лилии','символ элегантности, изысканности и романтики', 9000,'images\\blue_lilies.jpg')
+    ('Красные розы','небольшой букет вечной классики', 7500,'images/red_roses.jpg'),
+    ('Желтые тюльпаны','свежие, яркие тюльпына', 6500,'images/yellow_tulips.jpeg'),
+    ('Синие лилии','символ элегантности, изысканности и романтики', 9000,'images/blue_lilies.jpg')
 ])
+
+conn.commit()
+conn.close()
+
+print('База данных создана')
